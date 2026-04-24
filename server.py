a96c0665594a2fd0adf5ed131ceeb476a78deeaa
Lucas Iglesias <119307150+Lucas7500@users.noreply.github.com>
2026-04-24 00:41:25 +0000

    feat: initial commit

--- server.py
+++ server.py
@@ -3,13 +3,30 @@ import Demo
  
 class PrinterI(Demo.Printer):
     def printString(self, s, current=None):
-        print(s)
+        print(f"Printer: {s}")
 
-communicator = Ice.initialize(sys.argv) 
+    def add(self, a, b, current=None):
+        return a + b
 
-adapter = communicator.createObjectAdapterWithEndpoints("SimpleAdapter", "default -p 11000")
-object = PrinterI()
-adapter.add(object, communicator.stringToIdentity("SimplePrinter"))
-adapter.activate()
+    def toUpper(self, s, current=None):
+        return s.upper()
 
-communicator.waitForShutdown()
+class CalculatorI(Demo.Calculator):
+    def add(self, a, b, current=None):
+        return a + b
+    
+    def sub(self, a, b, current=None):
+        return a - b
+
+with Ice.initialize(sys.argv) as communicator:
+    adapter = communicator.createObjectAdapterWithEndpoints("SimpleAdapter", "default -p 11000")
+    
+    printer_obj = PrinterI()
+    adapter.add(printer_obj, communicator.stringToIdentity("SimplePrinter"))
+    
+    calc_obj = CalculatorI()
+    adapter.add(calc_obj, communicator.stringToIdentity("SimpleCalculator"))
+    
+    adapter.activate()
+    print("Servidor rodando...")
+    communicator.waitForShutdown()
